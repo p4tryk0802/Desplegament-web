583eec8a02810e1d65d8de983fe02042e7e2f0e4
p4tryk0802 <patrykpyczko@paucasesnovescifp.cat>
2024-01-07 18:51:33 +0100

    Modificant Nou.php

--- Nou.php
+++ Nou.php
@@ -1,5 +1,6 @@
 <?php
 
+    echo "Arxiu Nou.php creat per en Patryk Pyczko";
 /* 
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
